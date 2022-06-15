715e5c0820764a6c9ee6271134527139f760f036
Jacob Lawrence <jacob.lawrence@lightspeedhq.com>
2022-06-15 10:40:57 +1000

    configure ts for rollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,34 +1,11 @@
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import pkg from './package.json';
+// rollup.config.js
+import typescript from '@rollup/plugin-typescript';
 
-export default [
-    // browser-friendly UMD build
-    {
-        input: 'src/main.js',
-        output: {
-            name: 'main',
-            file: pkg.browser,
-            format: 'umd',
-        },
-        plugins: [
-            resolve(), // so Rollup can find `ms`
-            commonjs(), // so Rollup can convert `ms` to an ES module
-        ],
+export default {
+    input: 'src/index.ts',
+    output: {
+        dir: 'dist',
+        format: 'cjs',
     },
-
-    // CommonJS (for Node) and ES module (for bundlers) build.
-    // (We could have three entries in the configuration array
-    // instead of two, but it's quicker to generate multiple
-    // builds from a single configuration where possible, using
-    // an array for the `output` option, where we can specify
-    // `file` and `format` for each target)
-    {
-        input: 'src/main.js',
-        external: ['ms'],
-        output: [
-            { file: pkg.main, format: 'cjs' },
-            { file: pkg.module, format: 'es' },
-        ],
-    },
-];
+    plugins: [typescript({ compilerOptions: { module: 'esnext' } })],
+};
