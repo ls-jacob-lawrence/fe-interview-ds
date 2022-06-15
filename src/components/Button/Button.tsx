44b3d2cede1570b1df445c360d1693078fb7ea9d
Jacob Lawrence <jacob.lawrence@lightspeedhq.com>
2022-06-15 11:52:53 +1000

    init storybook

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,5 +1,28 @@
-import React from 'react';
+import React, { ButtonHTMLAttributes } from 'react';
 
-export function Button() {
-    return <button>hi</button>;
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+    children: JSX.Element;
+    disabled?: boolean;
+};
+
+const styles: React.CSSProperties = {
+    border: 'none',
+    borderRadius: '4px',
+    fontSize: '16px',
+    padding: '16px',
+    color: 'white',
+    backgroundColor: '#2E61DE',
+    cursor: 'pointer',
+    margin: '0',
+};
+export function Button({
+    children,
+    disabled = false,
+    ...restProps
+}: ButtonProps) {
+    return (
+        <button style={styles} disabled={disabled} {...restProps}>
+            {children}
+        </button>
+    );
 }
